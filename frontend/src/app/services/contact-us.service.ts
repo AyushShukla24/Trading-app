f5c1085d0ec8ee512474cef20edc43fe19451461
AyushShukla <ayshukla@lentra.ai>
2026-03-25 16:02:45 +0530

    Minor Changes

--- frontend/src/app/services/contact-us.service.ts
+++ frontend/src/app/services/contact-us.service.ts
@@ -2,6 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
 export class ContactUsService {
 
   constructor(private http:HttpClient) { }
